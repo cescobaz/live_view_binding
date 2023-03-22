0de19206919dbef324ba1c6544981592a3750b26
Francesco Burelli <francesco.burelli@protonmail.com>
2023-03-22 13:19:32 +0100

    add tests

--- test/live_view_binding_test.exs
+++ test/live_view_binding_test.exs
@@ -10,13 +10,35 @@ defmodule LiveViewBindingTest do
 
   @endpoint LiveViewBindingTest.Endpoint
 
-  test "greets the world" do
+  test "loader and updater" do
     conn = build_conn()
     path = Routes.live_path(conn, TestLiveView, %{key: "value"})
-    {:ok, view, html} = live(conn, path)
-    assert html =~ "loader(key=value,"
+    {:ok, view, _html} = live(conn, path)
+
+    assert view
+           |> element("#entity_a")
+           |> render() =~ "loader(key=value,"
+
+    assert view
+           |> element("#entity_b")
+           |> render() =~ "mapper_of(loader(key=value,"
+
+    assert view
+           |> element("#entity_c")
+           |> render() =~ "mapper(socket)"
 
     send(view.pid, :hello)
-    assert render(view) =~ "updater(hello)"
+
+    assert view
+           |> element("#entity_a")
+           |> render() =~ "updater(hello)"
+
+    assert view
+           |> element("#entity_b")
+           |> render() =~ "mapper_of(updater(hello))"
+
+    assert view
+           |> element("#entity_c")
+           |> render() =~ "mapper(socket)"
   end
 end
--- test/support/test_live_view.ex
+++ test/support/test_live_view.ex
@@ -4,17 +4,33 @@ defmodule LiveViewBindingTest.TestLiveView do
 
   def render(assigns) do
     ~H"""
-    <%= @resource_name %>
+    <div id="entity_a"><%= @entity_a %></div>
+    <div id="entity_b"><%= @entity_b %></div>
+    <div id="entity_c"><%= @entity_c %></div>
     """
   end
 
-  bind :resource_name do
+  bind :entity_a do
     loader(fn params, uri, _socket ->
       "loader(#{params |> URI.encode_query()}, #{uri})"
     end)
 
     updater(fn message, _socket ->
       "updater(#{message})"
+    end)
+  end
+
+  bind :entity_b do
+    mapper_of(:entity_a, fn entity_a ->
+      "mapper_of(#{entity_a})"
+    end)
+  end
+
+  bind :entity_c do
+    getter()
+
+    mapper(fn socket ->
+      get_current_entity_c(socket) || "mapper(socket)"
     end)
   end
 end
